cb2d6d2a85961a9b5279fe544cb1b90c5e44157b
gyu22 <ehtlfk2@naver.com>
2023-05-27 22:08:23 +0900

    첫번째 프레임 버튼추가
    mapview는 제한사항으로 인해 무조건 다른 하나의 창으로 띄워진다고 함.
    내일 첫번째 프레임에 모두 띄울수있도록 다시 시도해보겠음

--- getMap.py
+++ getMap.py
@@ -1,8 +1,10 @@
-#현재 좌표의 지도로 띄우기 - 첫번째 프레임에 띄우기 - 지도 배치확인
+#작은 문제점: 내 좌표가 실제좌표와 다름
+#첫번째 프레임에 띄우기 - 지도 배치확인
 #api_key = AIzaSyB2d8ZBeA7WoD3v4cbsgihUvC1IbmrrAIU
 
 from getLocation import *
-import webbrowser
+import tkinter as tk
+import webview
 import folium
 
 # 현재 좌표 정보 받아오기
@@ -10,12 +12,22 @@ location_data = get_currLocation()
 current_location = {'lat': location_data['geoplugin_latitude'], 'lng': location_data['geoplugin_longitude']}
 
 # 사용자의 Google Maps API 키
-api_key = "AIzaSyB2d8ZBeA7WoD3v4cbsgihUvC1IbmrrAIU"
+api_key = "YOUR_API_KEY"
 
 # 지도 생성
-m = folium.Map(location=[float(current_location['lat']), float(current_location['lng'])], zoom_start=15, API_key=api_key)
-folium.Marker(location=[float(current_location['lat']), float(current_location['lng'])], popup='현재 위치').add_to(m)
-m.save('map.html')
+m = folium.Map(location=[float(current_location['lat']), float(current_location['lng'])], zoom_start=15)
 
-# 지도 파일 열기
-webbrowser.open('map.html')
+# 현재 위치에 마커 추가
+folium.Marker(location=[float(current_location['lat']), float(current_location['lng'])], popup='현재 위치').add_to(m)
+
+# 지도를 HTML 파일로 저장
+m.save("map.html")
+
+# WebView 윈도우 실행
+def show_map():
+    webview.create_window("Interactive Map", "map.html", width=800, height=600, resizable=True)
+    webview.start()
+
+# WebView 실행 함수 호출
+# show_map()
+
